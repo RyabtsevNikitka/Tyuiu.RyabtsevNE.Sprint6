639693b2ea8b5934905b268192edc730ec8acd95
dlyav <github_090506@mail.ru>
2024-12-21 17:38:04 +0500

    Task5

---
+++ Tyuiu.RyabtsevNE.Sprint6.Task5.V22.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.RyabtsevNE.Sprint6.Task5.V22.Lib;
+
+namespace Tyuiu.RyabtsevNE.Sprint6.Task5.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.GetTempFileName();
+            FileInfo info = new FileInfo(path);
+            bool res = info.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
