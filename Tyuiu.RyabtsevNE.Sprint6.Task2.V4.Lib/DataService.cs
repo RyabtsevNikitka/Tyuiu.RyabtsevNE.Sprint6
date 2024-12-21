3ba58f0161c4d1e12a8318b39b8d39e1894e5f76
dlyav <github_090506@mail.ru>
2024-12-21 17:18:54 +0500

    Task2

---
+++ Tyuiu.RyabtsevNE.Sprint6.Task2.V4.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.RyabtsevNE.Sprint6.Task2.V4.Lib
+{
+    public class DataService : ISprint6Task2V4
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = stopValue - startValue + 1;
+            double[] res = new double[len];
+            int count = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                double y = Math.Round(((5 * i + 2.5) / (Math.Sin(i) + 2)) + 2 * i + 2, 2);
+
+
+
+                if (Math.Sin(i) == -2)
+                {
+                    res[i] = 0;
+                }
+                res[count] = y;
+
+                count++;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.RyabtsevNE.Sprint6.Task2.V4.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.RyabtsevNE.Sprint6.Task2.V4.Lib;
+
+namespace Tyuiu.RyabtsevNE.Sprint6.Task2.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int a = 0; int b = 0;
+            double[] res = ds.GetMassFunction(a, b);
+            double wail;
+            wail = 3.25;
+
+            Assert.AreEqual(res[0], wail);
+
+        }
+    }
+}
