c747dcac1019cc3bba5f89c89b0172e9ad03801d
dlyav <github_090506@mail.ru>
2024-12-21 17:30:37 +0500

    Task4

---
+++ Tyuiu.RyabtsevNE.Sprint6.Task4.V3.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.RyabtsevNE.Sprint6.Task4.V3.Lib
+{
+    public class DataService : ISprint6Task4V3
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            int len = stopValue - startValue + 1;
+            double[] result = new double[len];
+            int count = 0;
+            double res;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                if ((Math.Sin(i) - 3 + i) != 0)
+                {
+                    res = (3 * i - 1.5) / (Math.Sin(i) - 3 + i) + 2;
+                    result[count] = Math.Round(res, 2);
+                }
+                else
+                {
+                    result[count] = 0;
+                }
+                count++;
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.RyabtsevNE.Sprint6.Task4.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.RyabtsevNE.Sprint6.Task4.V3.Lib;
+
+namespace Tyuiu.RyabtsevNE.Sprint6.Task4.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int start = -1, end = 1;
+            double[] wait = new double[] { 2.93, 2.5, 0.71 };
+            double[] res = new double[ds.GetMassFunction(start, end).Length];
+            res = ds.GetMassFunction(start, end);
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
