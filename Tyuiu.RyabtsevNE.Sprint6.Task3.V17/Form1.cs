ab0164d065176dd95e10e567c5a663f311fbd23a
dlyav <github_090506@mail.ru>
2024-12-21 17:24:22 +0500

    Task3

---
+++ Tyuiu.RyabtsevNE.Sprint6.Task3.V17/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.RyabtsevNE.Sprint6.Task3.V17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
